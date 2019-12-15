3fb6bdf247e686e9edc4344b507bd8697b5a09b1
kuas-fyp <yasirahmedsiddiqui001@gmail.com>
2019-12-16 01:21:47 +0500

    initial

---
+++ precache-manifest.76ddcecafe967ad13ad965346f8f1d6d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "23236c51a9e838940dc00f5d8d098551",
+    "url": "/kuas-fyp.github.io/static/media/logoWide.23236c51.png"
+  },
+  {
+    "revision": "ab398af99ef4b5351a22",
+    "url": "/kuas-fyp.github.io/static/css/main.d62f0f5b.chunk.css"
+  },
+  {
+    "revision": "ac43448e68adcff3e77e",
+    "url": "/kuas-fyp.github.io/static/js/runtime~main.04b067e7.js"
+  },
+  {
+    "revision": "31fa6790eae064149e3e15e57b14efaa",
+    "url": "/kuas-fyp.github.io/static/media/ku.31fa6790.jpg"
+  },
+  {
+    "revision": "c879b2a6dd058f558c55",
+    "url": "/kuas-fyp.github.io/static/js/2.a0f99915.chunk.js"
+  },
+  {
+    "revision": "e95ed184777fcf7a8b5187bb06e07615",
+    "url": "/kuas-fyp.github.io/static/media/logo.e95ed184.png"
+  },
+  {
+    "revision": "f4883f421c60a6a5d90a418b4a74b841",
+    "url": "/kuas-fyp.github.io/static/media/logoWideBlue.f4883f42.png"
+  },
+  {
+    "revision": "ab398af99ef4b5351a22",
+    "url": "/kuas-fyp.github.io/static/js/main.1c321368.chunk.js"
+  },
+  {
+    "revision": "8e7fc2829885ebc18b4af8660bb7f138",
+    "url": "/kuas-fyp.github.io/static/media/alumni.8e7fc282.svg"
+  },
+  {
+    "revision": "fdb750b0ae806c55ff272cd5f0627318",
+    "url": "/kuas-fyp.github.io/static/media/admin.fdb750b0.svg"
+  },
+  {
+    "revision": "b43e383ee3573446c9cd192535e90972",
+    "url": "/kuas-fyp.github.io/static/media/department.b43e383e.svg"
+  },
+  {
+    "revision": "537860faa4bbc82c142be60183127959",
+    "url": "/kuas-fyp.github.io/static/media/faculty.537860fa.svg"
+  },
+  {
+    "revision": "066737613376bbbd84f3751c3db7d198",
+    "url": "/kuas-fyp.github.io/static/media/logoB.06673761.png"
+  },
+  {
+    "revision": "c879b2a6dd058f558c55",
+    "url": "/kuas-fyp.github.io/static/css/2.24612c6a.chunk.css"
+  },
+  {
+    "revision": "13b29f8fa2e24e88d395fd4e8ada5972",
+    "url": "/kuas-fyp.github.io/index.html"
+  }
+];
